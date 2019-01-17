0094b1cd48ccaecf02ac1cccf3dbf79047baee6a
anytaaly <anytarali@gmail.com>
2019-01-17 14:07:13 -0600

    commiting all counter messages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
-import Counter from "./components/Counter";
+import Counters from "./components/Counters";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Counter /> <br /> <Counter /> <br />
-        <Counter />
+        <Counters />
       </div>
     );
   }
